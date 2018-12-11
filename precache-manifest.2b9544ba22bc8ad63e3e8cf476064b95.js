35f6b98c51e7183c1ef1ef794cdff39f70fcb269
ewoken <ewokenfr@gmail.com>
2018-12-11 11:42:38 +0100

    Updates

---
+++ precache-manifest.2b9544ba22bc8ad63e3e8cf476064b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b5285d0891e06b293f1b",
+    "url": "/static/js/main.b5285d08.chunk.js"
+  },
+  {
+    "revision": "24f61847de32d37670ef",
+    "url": "/static/js/1.24f61847.chunk.js"
+  },
+  {
+    "revision": "b5285d0891e06b293f1b",
+    "url": "/static/css/main.7ef6d558.chunk.css"
+  },
+  {
+    "revision": "24f61847de32d37670ef",
+    "url": "/static/css/1.af532328.chunk.css"
+  },
+  {
+    "revision": "79a3db4167afb4cb04e0115b94f1a550",
+    "url": "/index.html"
+  }
+];
