16cbdec0d1867835fefee154664bc7720442f6e7
ewoken <ewokenfr@gmail.com>
2018-12-11 12:26:14 +0100

    Fix prepare build script

--- scripts/prepareBuild.sh
+++ scripts/prepareBuild.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir public/img
+mkdir public/img/flags
+mkdir public/geo
 cp node_modules/world-countries/data/*.svg public/img/flags
 cp node_modules/world-countries/data/*.geo.json public/geo
 cp CNAME public
