e223234646fbb36bd1f84a6b295762134eeb4bf5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-14 12:33:25 +0000

    Deploy ewoken/world-data-app to github.com/ewoken/world-data-app.git:gh-pages

---
+++ precache-manifest.7a2d67509fc419f26b74c27e18144e73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c510dfe533fcd19a2914",
+    "url": "/static/js/main.0805759e.chunk.js"
+  },
+  {
+    "revision": "e6954133dd295eb9055a",
+    "url": "/static/js/2.b52e2257.chunk.js"
+  },
+  {
+    "revision": "c510dfe533fcd19a2914",
+    "url": "/static/css/main.9e9bcd3f.chunk.css"
+  },
+  {
+    "revision": "e6954133dd295eb9055a",
+    "url": "/static/css/2.817a7e69.chunk.css"
+  },
+  {
+    "revision": "686b14467ef2b4b1db87cfa3aa704ba6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9b7ee6098a6bad68cfde279297a31ad.js"
+  "/precache-manifest.7a2d67509fc419f26b74c27e18144e73.js"
 );
 
 workbox.clientsClaim();
