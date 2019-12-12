2e529dcded2974c539c9a6d2d7f67653812eda2b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-12 20:13:27 +0000

    Deploy ewoken/world-data-app to github.com/ewoken/world-data-app.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b726e780a66795f766792f2e884911c6.js"
+  "/world-data-app/precache-manifest.2445b3f958c95da895e10ba7c2d02a3f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/world-data-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
